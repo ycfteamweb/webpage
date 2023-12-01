33f9e7922cc85babfea6a06fb593238d80e2da76
ycfteamweb <151065010+ycfteamweb@users.noreply.github.com>
2023-12-01 22:03:38 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Function to show the popup
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+document.addEventListener("DOMContentLoaded", function() {
+    // Show the popup when the page loads
+    showPopup();
+});
+
